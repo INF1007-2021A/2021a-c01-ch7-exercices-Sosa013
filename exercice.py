de885584156b17ed7c399cc772f3eeb6d225d26f
Sosa013 <89996378+Sosa013@users.noreply.github.com>
2021-10-21 15:55:57 -0700

    done 1

--- exercice.py
+++ exercice.py
@@ -2,13 +2,20 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
-
+import math
+from exerciceA import frequence
 
 # TODO: Définissez vos fonction ici
+def VolumeMasse(a, b, c, massevolumique):
 
+    V = 4/3 * (math.pi * a * b * c)
+    masse = massevolumique * V
+    tuple = (V, masse)
+    return tuple
+
+def frequence
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
+    print(VolumeMasse(1,2,3,5))
 
-    pass
